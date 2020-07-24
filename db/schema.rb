cad361fad15813449a1a483779d363940abd7d8f
Khalil Kum <kum.vanjunior@gmail.com>
2020-07-23 20:41:30 -0400

    Add user validation and update model attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_23_234219) do
+ActiveRecord::Schema.define(version: 2020_07_24_002602) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_234219) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
