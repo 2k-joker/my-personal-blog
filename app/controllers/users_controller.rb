aa497292fba4732f4bbbe95fd36259b7604a4cb0
Khalil Kum <kum.vanjunior@gmail.com>
2020-07-24 02:49:49 -0400

    Add user gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
   def update
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 100 })
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}/?s=200"
+    image_tag(gravatar_url, alt: user.username, class: "shadow rounded mx-auto d-block", id: "gravatar")
+  end
 end
