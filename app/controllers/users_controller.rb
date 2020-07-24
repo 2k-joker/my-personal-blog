5404d12890cd9bcaf933f794e9f3873e9032afc3
Khalil Kum <kum.vanjunior@gmail.com>
2020-07-23 23:20:22 -0400

    Add user account update functionality and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(whitelist_user_params)
+      flash[:notice] = "Account successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+    
+  end
+  
   private
 
   def whitelist_user_params
