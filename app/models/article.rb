e8837db7f7b2dfb63fc75810bbb0dc219ab62271
Khalil Kum <kum.vanjunior@gmail.com>
2020-08-07 20:17:39 -0400

    Add search article by author feature and update nav bar

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,6 +12,15 @@ class Article < ApplicationRecord
     where("#{field_name} like ?", "%#{param}%")
   end
 
+  def self.article_user_matches(field_name, param)
+    joins(:user).where("users.#{field_name} LIKE ?", "%#{param}%")
+  end
+
+  def self.match_by_author(param)
+    (article_user_matches('username', param) + 
+      article_user_matches('email', param)).uniq
+  end
+
   def self.title_matches(param)
     matches('title', param)
   end
@@ -22,7 +31,9 @@ class Article < ApplicationRecord
 
   def self.search(param)
     param.strip!
-    query_result = (title_matches(param) + description_matches(param)).uniq
+    query_result = (title_matches(param) + description_matches(param) +
+      match_by_author(param)).uniq
+
     return nil unless query_result
     query_result
   end
